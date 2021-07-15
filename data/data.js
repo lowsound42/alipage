4cd0d65937178e617aad13434de1a0050b0f565a
Omar Khan <o.khandxb@gmail.com>
2021-07-15 19:20:52 -0400

    added new song

--- data/data.js
+++ data/data.js
@@ -10,24 +10,21 @@ export const data = {
         {
             widget: 990243298,
             client: 'Jayel',
-            review:
-                '“Highly recommended! He finished a bundle 20 tracks within a week! Tracks are all perfect and the price was also awesome! He is very friendly and does his best to satisfy you! I hope we will work together again soon” - Jayel',
+            review: '“Highly recommended! He finished a bundle 20 tracks within a week! Tracks are all perfect and the price was also awesome! He is very friendly and does his best to satisfy you! I hope we will work together again soon” - Jayel',
             type: 2,
             genre: 'synth'
         },
         {
             widget: 990243271,
             client: 'Tschokee',
-            review:
-                '“Quick and high quality service! Absolutely surpassed my expectations.” - Tschokee',
+            review: '“Quick and high quality service! Absolutely surpassed my expectations.” - Tschokee',
             type: 1,
             genre: 'synth'
         },
         {
             widget: 990243280,
             client: 'George Alexiou',
-            review:
-                '“Excellent service and very professional” - George Alexiou',
+            review: '“Excellent service and very professional” - George Alexiou',
             type: 1,
             genre: 'synth'
         },
@@ -43,24 +40,21 @@ export const data = {
         {
             widget: 990199777,
             client: 'Yuyuwahyudi',
-            review:
-                '“Highly recommended. good communication, I really like the way he works. Very friendly, I will be back” - Yuyuwahyudi',
+            review: '“Highly recommended. good communication, I really like the way he works. Very friendly, I will be back” - Yuyuwahyudi',
             type: 4,
             genre: 'folk'
         },
         {
             widget: 995238406,
             client: 'Hellollerre',
-            review:
-                '“Outstanding!! He made a backing track for my song in less than a day, with really excellent communication. Went absolutely above and beyond the description.” - Hellollerre',
+            review: '“Outstanding!! He made a backing track for my song in less than a day, with really excellent communication. Went absolutely above and beyond the description.” - Hellollerre',
             type: 2,
             genre: 'folk'
         },
         {
             widget: 990199870,
             client: 'Kamilsalem',
-            review:
-                '“A music arranger who is very patient and can produce good work.” - Kamil Salem',
+            review: '“A music arranger who is very patient and can produce good work.” - Kamil Salem',
             type: 1,
             genre: 'folk'
         }
@@ -69,24 +63,28 @@ export const data = {
         {
             widget: 990237871,
             client: 'Itsmepzrk',
-            review:
-                '“Great job and excellent communication! Thanks a lot for your work” - Itsmepzrk',
+            review: '“Great job and excellent communication! Thanks a lot for your work” - Itsmepzrk',
+            type: 1,
+            genre: 'Pop/Rock'
+        },
+        {
+            widget: 1088324680,
+            client: 'Hellollerre',
+            review: '“Fantastic job. Thank you! - Hellollerre',
             type: 1,
             genre: 'Pop/Rock'
         },
         {
             widget: 990237880,
             client: 'Destineddancesb',
-            review:
-                '“Wonderful experience! Communication was excellent and the finished track was exactly what I was hoping for. I highly recommend!” - Destineddancesb',
+            review: '“Wonderful experience! Communication was excellent and the finished track was exactly what I was hoping for. I highly recommend!” - Destineddancesb',
             type: 2,
             genre: 'Pop/Rock'
         },
         {
             widget: 990237868,
             client: 'Strawberrysiren',
-            review:
-                '“Absolutely amazing, listened to what I want and super nice!” - Strawberrysiren',
+            review: '“Absolutely amazing, listened to what I want and super nice!” - Strawberrysiren',
             type: 1,
             genre: 'Pop/Rock'
         }
@@ -141,8 +139,7 @@ export const data = {
         {
             widget: 990235762,
             client: 'Bhn Pres',
-            review:
-                '“The track has three different elements of what I could use for my podcast the Producer was very responsive to all of my feedback” - Bhn Pres',
+            review: '“The track has three different elements of what I could use for my podcast the Producer was very responsive to all of my feedback” - Bhn Pres',
             type: 3,
             genre: 'Misc'
         }
